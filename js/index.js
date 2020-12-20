6205d2793c0c7fbf000d6a8d269ebbacd3732f84
EGavinG <edwardgavingarcia@yahoo.com>
2020-12-19 19:41:05 -0600

    J query add

--- js/index.js
+++ js/index.js
@@ -1,24 +1,27 @@
-$(".button").hover(
-  function() {
-    $( this ).css({
-      background: '#FAF8F4', 
-      color: '#FAF8F4',
-      '-webkit-text-stroke-width': '2px',
-      '-webkit-text-stroke-color': '#2B2B2B'
-    });
-  }, function() {
-    $( this ).css({ 
-      background: '#FAF8F4', 
-      color: '#2B2B2B' 
-    });
-  }
-);
+$(document).ready(function () {
+  $(".button").hover(
+    function () {
+      $(this).css({
+        background: '#FAF8F4',
+        color: '#FAF8F4',
+        '-webkit-text-stroke-width': '2px',
+        '-webkit-text-stroke-color': '#2B2B2B'
+      });
+    }, function () {
+      $(this).css({
+        background: '#FAF8F4',
+        color: '#2B2B2B'
+      });
+    }
+  );
 
-$(".button").click(
-  function() {
-    $( this ).css({ 
-      background: '#2B2B2B', 
-      color: '#FAF8F4'
-    });
-  }
-);
+  $(".button").click(
+    function () {
+      $(this).css({
+        background: '#7C7C79',
+        color: '#FAF8F4'
+      });
+    }
+  );
+});
+
